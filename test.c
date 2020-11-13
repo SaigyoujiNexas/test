d6abd055ae831e5bad89aca51abfacb963f2da53
Saigyoujinexas <73990376+Saigyoujinexas@users.noreply.github.com>
2020-11-13 09:43:27 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
-int main(void)
+int main()
 {
 	int a[10];
 	int b, i;
-	for(i = 0; i < 10; i++）
+	for (i = 0; i < 10; i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d", &a[i]);
 		getchar();
 	}
-	for(i = 1; i < 10; i++)
+	for (i = 1; i < 10; i++)
 	{
-		for(b = i - 1;b >= 0; b--)
-		
-			if(a[b] < a[i])
+		for (b = i - 1; b >= 0; b--)
+		{
+			if (a[b] < a[i])
 			{
 				a[b] = a[b] + a[i];
 				a[i] = a[b] - a[i];
 				a[b] = a[b] - a[i];
-				}
-		
+			}
+		}
+			for (i = 0; i < 10; i++)
+			{
+				printf("%d ", a[i]);
+			}
+		return 0;
 	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ",a[i]);
-	}
-	return 0;
 }
