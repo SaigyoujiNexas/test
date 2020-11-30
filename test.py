2044bef5d190f521ce751990f9e7afb74f943b7c
Saigyoujinexas <73990376+Saigyoujinexas@users.noreply.github.com>
2020-11-30 20:24:47 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = n
+b = 0
+for i in range(n):
+        for x in range(b):
+                print(" ")
+        for y in range(a):
+                print("*")
+        print("\n")
+        a -= 1
+        b += 1
