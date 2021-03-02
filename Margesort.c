42ab425d8efea1dc1a0ff891a2a3b2dd6823c810
Saigyoujinexas <73990376+Saigyoujinexas@users.noreply.github.com>
2021-03-02 17:14:06 +0800

    Create Margesort.c

--- Margesort.c
+++ Margesort.c
@@ -1,60 +1,45 @@
-#include <stdio.h>
-#define N 5
-void sort(double num[], int start, int end);
-void process(double arr[], int left, int middle, int right);
-int main(void)
-{
-	double num[N];
-	for (int i = 0; i < N; i++)
-	{
-		scanf_s("%lf", &num[i]);
-		getchar();
-	}
-	sort(num, 0, N - 1);
-	for (int i = 0; i < N; i++)
-		printf("%.1lf ", num[i]);
-	return 0;
-}
-
-void sort(double num[],int start, int end)
+void margesort(double num[], int start, int end)//主递归程序
 {
 	int i;
 	if (start < end)
 	{
 		i = (start + end) / 2;
-		sort(num, start, i);
-		sort(num, i + 1, end);
+		margesort(num, start, i);//并行二叉树
+		margesort(num, i + 1, end);
 		process(num, start, i + 1, end);
 	}
 	return;
 }
 
-void process(double arr[], int left, int middle, int right)
+void process(double num[], int left, int middle, int right)
 {
-	double arruse[N] = {0};
+	double arr[N] = {0};
 	int i, j, k;
-	for (i = left, j = 0, k = middle; j <= (right - left); j++)
+	for (i = left, j = middle, k = 0; k <= (right - left); k++)
 	{
-		if (i == middle) /*当第一数组位置至末尾时*/
+		//第一数组分配完毕
+		if (i == middle)
 		{
-			arruse[j] = arr[k++];
+			arr[k] = num[j++];
 			continue;
 		}
-		if (k == right + 1) /*第二数组位置到末尾时*/
+		//第二数组分配完毕
+		if (j == right + 1)
 		{
-			arruse[j] = arr[i++];
+			arr[k] = num[i++];
 			continue;
 		}
-		if (arr[i] < arr[k]) /*第一数组位置大小小于第二数组位置大小时， 把第一数组数据挪入临时数组*/
-			arruse[j] = arr[i++];
+		//正常分配
+		if (num[i] < num[j])
+			arr[k] = num[i++];
 		else
-			arruse[j] = arr[k++];
+			arr[k] = num[j++];
 	}
-	/*临时数组数据刷入源数组*/
-	for (i = 0, j = left; j <= right; j++, i++)
+	//刷入
+	for (i = 0, j = left; j <= right; i++, j++)
 	{
-		printf("%.1lf ", arruse[i]);
-		arr[j] = arruse[i];
+		num[j] = arr[i];
+		
 	}
-	putchar('\n');
+		return;
 }
